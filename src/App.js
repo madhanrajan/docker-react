aab3dd38d63086347780011ec1e8ea0536534adc
Madhanrajan <madhanrajan.varadharajan@gmail.com>
2020-09-16 02:34:02 +0800

    text change

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to restart.
+          And then god said...
         </p>
         <a
           className="App-link"
